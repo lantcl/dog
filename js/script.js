35569628562efdca3c5796cadc5c096bf3f692b1
Cleo Lant <lantc@shernet.sheridancollege.ca>
2018-11-27 17:12:52 -0500

    second build

--- js/script.js
+++ js/script.js
@@ -1,23 +1,27 @@
 console.log("connected");
 
+//Time Display
+
 function startTime() {
     var today = new Date();
     var h = today.getHours();
     var m = today.getMinutes();
     m = checkTime(m);
+    tt = "AM";
 	if (h > 12){
-	h = h-12}
-    document.getElementById('datetime').innerHTML =
-    h + ":" + m;
-    var t = setTimeout(startTime, 30000);
-
-//console.log(h);
+	h = h-12
+    tt = "PM";
+    }
+    document.getElementById('datetime').innerHTML = h + ":" + m + " " + tt;
+    var t = setTimeout(startTime, 20000);
 }
 
 function checkTime(i) {
     if (i < 10) {i = "0" + i};  // add zero in front of numbers < 10
     return i;
 }
+
+//Date Display 
 
 var monthNames = ["January", "February", "March", "April", "May", "June",
   "July", "August", "September", "October", "November", "December"
@@ -27,3 +31,29 @@ var todaydate = new Date();
 var d = todaydate.getDate();
 var mo = monthNames[todaydate.getMonth()];
 document.getElementById("today").innerHTML = mo + " " + d;
+
+//Clock Buttons
+
+
+var forward = document.getElementById("goforward");
+forward.style.cursor= "pointer";
+forward.addEventListener("click", forwardfunction, false);
+function forwardfunction(){
+    console.log("clicked");
+}
+
+var back = document.getElementById("goback");
+back.style.cursor= "pointer";
+back.addEventListener("click", backfunction, false);
+function backfunction(){
+    console.log("clicked");        
+}
+
+//Add Walk
+
+var newWalk = document.getElementById("newalk");
+newalk.addEventListener("click", walkfunction, false);
+
+function walkfunction(){
+    display: block;
+}
--- main.php
+++ main.php
@@ -11,12 +11,15 @@ $pdo = new PDO($dsn, $dbusername, $dbpassword);
 
 $row = $pdo->prepare("SELECT * FROM `users` WHERE id = $userid");
 $row->execute();
+$user = $row->fetch();
 
 $stmt = $pdo->prepare("SELECT `id`, MAX(`time`) AS most_recent_walk FROM `walks` GROUP BY id");
 $stmt->execute();
 
 $lastwalk = $stmt->fetch();
-$user = $row->fetch();
+
+$last = $lastwalk["most_recent_walk"];
+$lastTime = date("H:m");
 
 ?>
 
@@ -29,36 +32,38 @@ $user = $row->fetch();
         <link rel="stylesheet" type="text/css" href="css/mobile.css">
     </head>
     <body onload="startTime()">
-        <header>
-        <section id="head">        
-            <a href = "main.php"><img id= "logo" src="assets/logo.svg" alt="walky talky logo"></a>
-            <h1>Last Walk <?php echo($lastwalk["most_recent_walk"]);?></h1>
+        <header>       
+            <h1><a id="logo" href = "main.php">Walky Talky</a></h1>
+            <h1>Last Walk <?php echo($lastTime);?></h1>
             <?php if($_SESSION['logged-in'] == true){?>
             <a href = "user-profile.php"><img id="usericon" src="assets/<?php echo($user["profilepic"]);?>" alt="profile icon"></a>
         <?php } else {?>
-            <h2><a href = "login.php"></a>Log In</h2>
+            <h1><a href = "login.php">Log In</a></h1>
         <?php } ?>
-        </section>
         </header>
-        <section id="nav">
-            <nav>
-                <ul>
-                <li><a href = "main.html">Home</a></li>
-                <li><a href = "walk-history.html">Walk History</a></li>
-                <li><a href = "login.html">Log in</a></li>
-                <li><a href = "signup.html">Sign up</a></li>
-                <li><a href = "featured-notice.html">Set Featured Notice</a></li>
-                </ul>
-            </nav>            
-        </section>
 
         <section id="subhead">         
+            <section class="dropdown">
+                <img id="menubutton" class= "arrowbutton" src="assets/menubutton.svg" alt="menuicon">
+                <nav class="dropdown-content">
+                    <ul>
+                    <li><a href = "main.html">Home</a></li>
+                    <li><a href = "walk-history.html">Walk History</a></li>
+                    <li><a id = "newwalk" href = "#">Add Walk</a></li>
+                    </ul>
+                </nav>          
+            </section>            
             <h3>Notice Goes here</h3>
             <h3><span id="datetime"></span></h3>
+
         </section>
         
         <section id ="main">
-            <h3><span id="today"></span></h3>
+            <div id="daynav">
+            <img id="goback" class= "arrowbutton" src="assets/backbutton.svg" alt="backbutton">
+            <h3 id="today"></h3>
+            <img id="goforward" class= "arrowbutton" src="assets/forwardbutton.svg" alt="forwardbutton">
+            </div>
             <div class = "clock">
                 <div class="middle">
                     <div class="circle"></div>
@@ -68,14 +73,9 @@ $user = $row->fetch();
                 <img id= "clockadd" src="assets/clockadd.svg" alt="walky talky clocky"> -->
             </div>
         </section>
-
-        <section id="side">
-            <p>Ad</p>
-        </section>
-
         <footer>
             <nav>
-                <ul>
+                <ul id="footernav">
                 <li><a href = "#">About Dog</a></li> 
                 <li><a href = "#">Contribute</a></li>
                 <li><a href = "#">Privacy Policy</a></li>  
