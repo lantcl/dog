c502ff2b2dcf0d71548f411a6178e342c776655f
Cleo Lant <lantc@shernet.sheridancollege.ca>
2018-12-05 15:38:32 -0500

    correct json output

--- walks.php
+++ walks.php
@@ -12,25 +12,16 @@ $date = date("Y-m-d");
 $stmt = $pdo->prepare("SELECT * FROM `walks` WHERE `date` = '$date'");
 $stmt->execute();
 
-$data = "[";
-while($rs = $stmt->fetch(PDO::FETCH_ASSOC)) {
-    if ($data != "[") {$data .= ",";}
-    $data .= '{"walktime":"'  . $rs["walktime"] . '",';  ///w3 schools
-    $data .= '"poo":"'   . $rs["poo"]   . '",';
-    $data .= '"id":"'. $rs["id"]     . '"}'; 
-}
-$data .="]";
-
-
-echo($data);
-
-// $date = new DateTime($lastwalk["time"]);
-// $tt = "AM";
-// if ($date > '12:00:00'){$tt = "PM";} 
-
-// $last = $date->format('H:i');
-
-
-// echo($walk["id"]);
+// $data = "[";
+// while($rs = $stmt->fetch(PDO::FETCH_ASSOC)) {
+//     if ($data != "[") {$data .= ",";}
+//     $data .= '{"walktime":"'  . $rs["walktime"] . '",';  ///w3 schools
+//     $data .= '"poo":"'   . $rs["poo"]   . '",';
+//     $data .= '"id":"'. $rs["id"]     . '"}'; 
+// }
+// $data .="]";
+$results = $stmt->fetchAll(PDO::FETCH_ASSOC);
+$json = json_encode($results);
+echo($json);
 
 ?>
